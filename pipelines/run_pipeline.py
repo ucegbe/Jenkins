6f0708902fed0d7e90535d409675cd09fdefb4f1
Uchenna <ucheegbe@amazon.com>
2023-11-30 20:38:16 +0000

    update

--- pipelines/run_pipeline.py
+++ pipelines/run_pipeline.py
@@ -71,7 +71,7 @@ def main():  # pragma: no cover
     if args.module_name is None:
         parser.print_help()
         sys.exit(2)
-    # tags = convert_struct(args.tags)
+    tags = convert_struct(args.tags)
 
     try:
         pipeline = get_pipeline_driver(args.module_name, args.kwargs)
